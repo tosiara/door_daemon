df60e1a190b0ab16ad76126493cda20bdcf920dd
tosiara <tosiara@users.noreply.github.com>
2017-04-18 17:07:13 +0300

    Added wiringPi module

--- gpio.c
+++ gpio.c
@@ -3,14 +3,20 @@
 #include <unistd.h>
 #include <syslog.h>
 
+#include <wiringPi.h>
+
+#define DOOR_PIN 6
+
 void gpio_init()
 {
-
+	syslog (LOG_NOTICE, "wiringPiSetup");
+	wiringPiSetup();
+	syslog (LOG_NOTICE, "Setting pin %d to INPUT", DOOR_PIN);
+	pinMode (DOOR_PIN, INPUT);
 }
 
 int gpio_read()
 {
-
-	return 0;
+	return digitalRead (DOOR_PIN);
 }
 
