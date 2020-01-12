f34e1acb00a5786b644f7cd255dd3f1ea7afee22
tosiara <tosiara@users.noreply.github.com>
2020-01-12 11:40:23 +0200

    Parapin output pin

--- gpio.c
+++ gpio.c
@@ -6,25 +6,38 @@
 #include "parapin.h"
 
 unsigned int DOOR_PIN = 0;
+unsigned int OUT_PIN = 0;
 
-void gpio_init (unsigned int config_pin)
+void gpio_init (unsigned int config_pin, unsigned int out_pin)
 {
-	DOOR_PIN = config_pin;
-
 	if (pin_init_user(LPT1) < 0)
 		exit(1);
 
-	pin_input_mode(LP_DATA_PINS);
-	pin_input_mode(LP_SWITCHABLE_PINS);
+	if (config_pin)
+	{
+		DOOR_PIN = LP_PIN[config_pin];
+		pin_input_mode(DOOR_PIN);
+	}
+
+	if (out_pin)
+	{
+		OUT_PIN = LP_PIN[out_pin];
+		pin_output_mode(OUT_PIN);
+		clear_pin (OUT_PIN);
+	}
 }
 
 int gpio_read()
 {
-	return pin_is_set(LP_PIN[DOOR_PIN]);
+	return pin_is_set (DOOR_PIN);
 }
 
 int gpio_write(int state)
 {
+	if (state)
+		set_pin (OUT_PIN);
+	else
+		clear_pin (OUT_PIN);
 
 	return 0;
 }
